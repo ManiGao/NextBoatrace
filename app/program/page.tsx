d3b0f909a12abcb6ab2d2323e0fd8507c7373be8
長尾慶伸 <manimon@nagaokeishinnoMacBook-Air.local>
2025-12-07 16:04:26 +0900

    For some reason, page.tsx isn't being recognized as a module, so I added a comment.

--- app/program/page.tsx
+++ app/program/page.tsx
@@ -4,7 +4,7 @@ export default function ProgramPage() {
   return (
     <main className="p-6">
       <h1 className="text-xl font-bold mb-4">今日の番組表</h1>
-      <p>ここに API から取得したデータを表示します。</p>
+      <p>ここに API から取得したデータを表示します。解決ゾロリ</p>
     </main>
   );
 }
